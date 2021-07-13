1a440567522b516840c53a6522e5ae6f7f5bbe15
Илья <ilyasoooper@gmail.com>
2021-07-13 11:42:57 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+"use strict"; //строгий режим !!!
+//let number = 5;
+//alert ("Sas");
+//const question = confirm ("Are you Sas?"); //вопрос да/нет
+//const answer = prompt ("do tou like sass?", "sas is my life"); //вопрос с текстовым ответом от пользователя
+//console.log(answer);
+
+const username = "Sanya";
+alert(`Hello, ${username}`);// ${ } - интерполяция вместо конкатенации 
+
