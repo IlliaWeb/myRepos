89eaed98b52876daf23d785b4f518330bdb53ff7
Illia <ilyasoooper@gmail.com>
2021-07-21 14:19:58 +0300

    first stage of learn js

--- js/script.js
+++ js/script.js
@@ -5,7 +5,28 @@
 //const answer = prompt ("do tou like sass?", "sas is my life"); //вопрос с текстовым ответом от пользователя
 //console.log(answer);
 
-const username = "Sanya";
-alert(`Hello,user ${username}`);// ${ } - интерполяция вместо конкатенации 
-prompt("you are like sas ?"," yes!!!");
+//const username = "Sanya";
+//alert(`Hello,user ${username}`);// ${ } - интерполяция вместо конкатенации 
+
+
+const numberOfFilms = +prompt('сколько фильмов вы уже посмотрели?','');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},//в свойствах пустой обьект
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('какой фильм вы смотрели последним?',''),
+      b = prompt('во сколько балов оцените фильм (1-10)?',''),
+      c = prompt('какой фильм вы смотрели последним?',''),
+      d = prompt('во сколько балов оцените фильм (1-10)?','');
+    
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB); 
+
 
